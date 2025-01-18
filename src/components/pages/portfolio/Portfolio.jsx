202af08389ff899eaa25d231f41dc18451998769
saddiq musa <saddiq0musa@gmail.com>
2025-01-18 18:46:29 +0300

    Refactor Portfolio and PageTitle components; simplify layout in Portfolio and enhance PageTitle animations with blur effects

--- src/components/pages/portfolio/Portfolio.jsx
+++ src/components/pages/portfolio/Portfolio.jsx
@@ -3,17 +3,9 @@ import { motion } from "motion/react";
 import PageTitle from "../../reusble-components/PageTitle";
 
 const Portfolio = () => {
-
-    return (
-        <motion.div
-            className="w-screen h-screen p-28"
-            initial={{ backgroundColor: "#fff" }}
-            whileInView={{ backgroundColor: "#0d2b4c" }}
-            transition={{ delay: 1, transition: 1.5, }}
-        >
-            <PageTitle />
-        </motion.div>
-    );
+    return <div className="relative w-screen h-screen p-28">
+        
+    </div>;
 };
 
 export default Portfolio;
--- src/components/reusble-components/PageTitle.jsx
+++ src/components/reusble-components/PageTitle.jsx
@@ -3,7 +3,12 @@ import { motion } from "motion/react";
 
 const PageTitle = ({ title = "هات عنوانك", color = "secondary" }) => {
     return (
-        <motion.h1 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ delay: 0.3, duration: 1 }} className={`text-${color} text-5xl font-500`}>
+        <motion.h1
+            initial={{ opacity: 0, filter: "blur(10px)" }}
+            whileInView={{ opacity: 1, filter: "blur(0px)" }}
+            transition={{ delay: 0.3, duration: 1 }}
+            className={`relative top-0 text-[150px] font-900 opacity-45 ${color === "secondary" ? "text-secondary" : `text-${color}`}`}
+        >
             {title}
         </motion.h1>
     );
