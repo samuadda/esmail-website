e095ba7b6ff05d45142838d9cba90d75e3ed8221
saddiq musa <saddiq0musa@gmail.com>
2025-01-19 20:22:34 +0300

    Update PageTitle component to use h3 tag; enhance Portfolio layout with BackgroundBeams and dynamic Card components

--- src/components/pages/Hero/NavBar.jsx
+++ src/components/pages/Hero/NavBar.jsx
@@ -1,5 +1,7 @@
 import InteractiveHoverButton from "./interactive-hover-button";
 import { motion } from "motion/react";
+import { FollowerPointerCard } from "../../ui/following-pointer";
+
 
 const NavBar = () => {
     const navItems = [
@@ -11,7 +13,12 @@ const NavBar = () => {
 
 
     return (
-        <motion.nav initial={{top:'-82px'}} animate={{top:0}} transition={{duration:1, type:'tween'}} className="w-full flex justify-between items-center px-28 py-5 top-0 fixed z-50 backdrop-blur-md bg-white/30 border-b border-white/20">
+        <motion.nav
+            initial={{ top: "-82px" }}
+            animate={{ top: 0 }}
+            transition={{delay:0.5, duration: 0.8, type: "tween" }}
+            className="w-full flex justify-between items-center px-28 py-5 top-0 fixed z-50 backdrop-blur-md bg-white/30 border-b border-white/20"
+        >
             <div>
                 <ul className="flex gap-10 text-lg items-center">
                     {navItems.map(item => {
@@ -22,12 +29,14 @@ const NavBar = () => {
                             </li>
                         );
                     })}
-                    <li className="relative text-primary font-400">
-                        <span class="absolute top-0 right-0 -mt-2 -mr-2 bg-secondary text-white text-xs font-bold px-3 py-1 rounded-sm before:content-['قريباً'] before:absolute before:inset-0 before:flex before:items-center before:justify-center before:text-white before:font-bold before:leading-none" />
-                        <a href="#" className="cursor-not-allowed opacity-45">
-                            المدونة
-                        </a>
-                    </li>
+                    <FollowerPointerCard title="اصبر علينا">
+                        <li className="relative text-primary font-400">
+                            <span class="absolute top-0 right-0 -mt-2 -mr-2 bg-secondary text-white text-xs font-bold px-3 py-1 rounded-sm before:content-['قريباً'] before:absolute before:inset-0 before:flex before:items-center before:justify-center before:text-white before:font-bold before:leading-none" />
+                            <a href="#" className="cursor-none opacity-45">
+                                المدونة
+                            </a>
+                        </li>
+                    </FollowerPointerCard>
                 </ul>
             </div>
             <div className="flex items-center gap-3">
--- src/components/reusble-components/PageTitle.jsx
+++ src/components/reusble-components/PageTitle.jsx
@@ -3,14 +3,14 @@ import { motion } from "motion/react";
 
 const PageTitle = ({ title = "هات عنوانك", color = "secondary" }) => {
     return (
-        <motion.h1
-            initial={{ opacity: 0, filter: "blur(10px)" }}
-            whileInView={{ opacity: 1, filter: "blur(0px)" }}
-            transition={{ delay: 0.3, duration: 1 }}
-            className={`relative top-0 text-[150px] font-900 opacity-45 ${color === "secondary" ? "text-secondary" : `text-${color}`}`}
+        <motion.h3
+            // initial={{ x: 0 }}
+            // whileInView={{ x: 5}}
+            // transition={{ delay: 0.3, duration: 1 }}
+            className={`relative top-0 text-center text-6xl font-900 ${color === "secondary" ? "text-secondary" : `text-${color}`}`}
         >
             {title}
-        </motion.h1>
+        </motion.h3>
     );
 };
 
