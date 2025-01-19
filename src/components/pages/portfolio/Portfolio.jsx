3dd8dc2b338390b21c236afa1607ec7fb2cfe9f7
saddiq musa <saddiq0musa@gmail.com>
2025-01-19 20:31:28 +0300

    Update Card component in Portfolio; adjust number display and enhance styling for better visual appeal

--- src/components/pages/portfolio/Portfolio.jsx
+++ src/components/pages/portfolio/Portfolio.jsx
@@ -5,11 +5,11 @@ import PageTitle from "@/components/reusble-components/PageTitle";
 
 const Portfolio = () => {
 
-    const Card = ({title = "عنوان", number = "32+"}) => {
+    const Card = ({title = "عنوان", number = "16"}) => {
         return (
             <div className="flex flex-col justify-center items-center">
-                <h4 className="p-3 text-primary text-3xl font-500 border border-primary  "> {number}</h4>
-                <span> {title}</span>
+                <h4 className="p-2 text-primary text-3xl font-500  border-primary border-4 rounded-lg  "> {`${number}+`}</h4>
+                <span className="bg-accent text-white py-1 px-3 rounded-md"> {title}</span>
             </div>
         );
     }
@@ -22,7 +22,7 @@ const Portfolio = () => {
         <div className="relative w-screen h-screen p-28 ">
             <BackgroundBeams />
             <PageTitle title="أكثر من..." />
-            <div className="flex flex-col items-center gap-40 p-20">
+            <div className="flex flex-col items-center gap-28 p-28">
                 <div className="flex w-full justify-around ">
                     <Card />
                     <Card />
